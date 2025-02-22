8408345606a39aef8de39d5ab1e005f7d357e21a
michaelgameel5 <michaelgameel15@gmail.com>
2025-02-22 18:28:28 +0200

    Example One

--- WebSecService/resources/views/welcome.blade.php
+++ WebSecService/resources/views/welcome.blade.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Michael</title>
+    <link href="public/css/bootstrap.min.css" rel="stylesheet">
+    <script src="public/js/bootstrap.bundle.min.js"></script>  
 </head>
 <body>
     <h1>Michael Gameel</h1>
+    <div class="card m-4">
+        <div class="card-header">Basic Web Page with Bootstrap</div>
+        <div class="card-body">Lorem ipsum dolor ...</div>
+    </div>
 </body>
 </html>
