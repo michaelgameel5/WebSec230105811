afa0cc53e8cbda939c18bf8043bd4611343468c7
michaelgameel5 <michaelgameel15@gmail.com>
2025-02-22 22:24:30 +0200

    Mixing Native PHP and Blade

--- WebSecService/resources/views/welcome.blade.php
+++ WebSecService/resources/views/welcome.blade.php
@@ -8,10 +8,53 @@
     <script src="public/js/bootstrap.bundle.min.js"></script>  
 </head>
 <body>
+    <?php
+    function isPrime($number){
+        if ($number<=1) return false;
+        $i = $number - 1;
+        while ($i > 1){
+            if ($number % $i ==0) return false;
+            $i--;
+            }
+        return true;
+    }
+    ?>
+    @php ($j=5)
     <h1>Michael Gameel</h1>
-    <div class="card m-4">
-        <div class="card-header">Basic Web Page with Bootstrap</div>
-        <div class="card-body">Lorem ipsum dolor ...</div>
+    <div class="card m-4 col-sm-2">
+        <div class="card-header">Multiplication Table 5</div>
+        <div class="card-body">
+            <table>
+                @foreach (range(1,10) as $i)
+                <tr><td>{{$i}} * {{$j}} = </td><td>{{$i * $j}}</td></tr>
+                @endforeach
+            </table>
+        </div>
+        <div class="card m-4">
+            <div class="card-header">Even Numbers</div>
+            <div class="card-body">
+                @foreach (range(1,100) as $i)
+                    @if ($i%2==0)
+                        <span class="badge bg-primary">{{$i}}</span>
+                    @else
+                        <span class="badge bg-secondary">{{$i}}</span>
+                    @endif
+                @endforeach
+            </div>
+            <div class="card m-4">
+                <div class="card-header">Prime Numbers</div>
+                <div class="card-body">
+                    @foreach (range(1,100) as $i)
+                        @if (isPrime($i))
+                            <span class="badge bg-primary">{{$i}}</span>
+                        @else
+                            <span class="badge bg-secondary">{{$i}}</span>
+                        @endif
+                    @endforeach
+                </div>
+            </div>
+
+        </div>
     </div>
 </body>
 </html>
